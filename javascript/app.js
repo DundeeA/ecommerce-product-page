97297f2af2753fb0c700e19c4ed1a357ea8048ee
dun <DundeeDevelops@gmail.com>
2022-09-11 11:43:22 -0500

    Cart functionality

---
+++ javascript/app.js
@@ -0,0 +1,59 @@
+// Toggle nav
+$(".nav-btn").click(function(){
+ //open nav
+    $('nav').css('display', 'flex');
+    $('body').css('overflow', 'hidden')
+    $('nav').animate({
+        left: "0",
+        opacity: '100%'
+    }, 800);
+})
+
+$(".close-nav-btn").click(function(){
+    //close nav
+    $('nav').animate({
+        left: "-60vw",
+        opacity: '0'
+    }, 800, function(){
+        $('nav').css('display', 'none');
+        $('body').css('overflow', 'visible')
+    });
+
+})
+
+// Toggle cart menu
+$(".cart-btn").click(function(){
+        $(".cart-menu").fadeToggle();
+});
+
+//Adding quantity
+let quantity = 0;
+$(".increase-btn").click(function(){
+   quantity++;
+   $('.qtty').html(quantity);
+});
+$(".decrease-btn").click(function(){
+    if(quantity === 0) return;
+    quantity--;
+    $('.qtty').html(quantity);
+});
+
+//Adding items to cart
+$(".add-cart-btn").click(function(){
+   if(quantity === 0) return;
+   //Add items to cart
+   $('.empty-text').hide();
+   $('.cart-item').css('display', 'flex');
+   $('.checkout-btn').show();
+
+   $('.price-calc').text(`$125 x ${quantity}`);
+   $('.final-price').text(`$${125 * quantity}`);
+   quantity = 0;
+   $('.qtty').html(quantity);
+});
+
+$('.remove-btn').click(function(){
+    $('.empty-text').show();
+    $('.cart-item').hide();
+    $('.checkout-btn').hide();
+});
